240cc5e6711bd5bd22349b531e03103cc791b51d
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-06-23 21:27:15 +0100

    Fix invalid js identifier name

--- src/Graphics/Vty/Platform/Wasi/Pty.hs
+++ src/Graphics/Vty/Platform/Wasi/Pty.hs
@@ -89,18 +89,18 @@ installPtySignalHandler pty h = evalContT $ do
   dispose <- ContT $ bracketOnError (js_pty_on_signal pty h1 h2 h3 h4) (freeJSVal . coerce)
   pure (js_call_jsval dispose >> mapM_ (freeJSVal . coerce) [h1, h2, h3, h4])
 
--- | Get the 'Pty' given its identifier in globalThis.vty-wasi
+-- | Get the 'Pty' given its identifier in globalThis.vty_wasi
 getPty :: String -> IO (Either String Pty)
 getPty ident = evalContT $ runExceptT do
 
-  jsVtyWasi <- lift . ContT $ bracket (withJSString "vty-wasi" js_get_global) freeJSVal
+  jsVtyWasi <- lift . ContT $ bracket (withJSString "vty_wasi" js_get_global) freeJSVal
   jsVtyWasi' <- (liftIO $ toJsObject jsVtyWasi) >>= \case
-    Left ty -> throwE $ "globalThis.vty-wasi expected to be an object, but is " ++ ty
+    Left ty -> throwE $ "globalThis.vty_wasi expected to be an object, but is " ++ ty
     Right o -> pure o
 
   slavePtyClass <- lift . ContT $ bracket (withJSString "Slave" (js_index_object jsVtyWasi')) freeJSVal
   slavePtyClass' <- (liftIO $ toJsObject slavePtyClass) >>= \case
-    Left ty -> throwE $ "globalThis.vty-wasi.Slave expected to be an object, but is " ++ ty
+    Left ty -> throwE $ "globalThis.vty_wasi.Slave expected to be an object, but is " ++ ty
     Right o -> pure o
 
   jsPty <- lift . ContT $ bracket (withJSString ident (js_index_object jsVtyWasi')) freeJSVal
@@ -108,7 +108,7 @@ getPty ident = evalContT $ runExceptT do
   b <- liftIO $ js_instanceof jsPty slavePtyClass'
   if b
     then pure $ coerce jsPty
-    else throwE $ "globalThis.vty-wasi." ++ ident ++ " must be an instance of globalThis.vty-wasi.Slave"
+    else throwE $ "globalThis.vty_wasi." ++ ident ++ " must be an instance of globalThis.vty_wasi.Slave"
 
 -- | Copies at most 'n' bytes of the JSByteArray to the given
 -- location in memory starting from index 0.
