bec4615b5b46ef9f9e9f8b9ebd454e1777b0021d
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-06-28 22:36:06 +0100

    WIP use JSFd (non-compiling)

--- src/Graphics/Vty/Platform/Wasi/Pty.hs
+++ src/Graphics/Vty/Platform/Wasi/Pty.hs
@@ -7,36 +7,26 @@
 {-# LANGUAGE LambdaCase #-}
 
 module Graphics.Vty.Platform.Wasi.Pty
-  ( Pty(..)
-  , getPty
-  , getWindowSize
+  ( getWindowSize
   , Termios(..)
   , getTermios
   , setTermios
-  , installPtySignalHandler
   , pattern ICANON
   , pattern ISIG
   , pattern ECHO
   , pattern IEXTEN
   , pattern ICRNL
   , pattern IXON
-  , Signal(..)
   ) where
 
-import GHC.IO.Device
 import Data.Word
 import Data.Coerce
-import Control.Monad
-import Control.Concurrent.MVar
 import Foreign.Ptr
 import Graphics.Vty.Platform.Wasi.Pty.JSFFI
 import Control.Exception
 import Data.Primitive.ByteArray
 import Foreign (allocaBytes)
-import Control.Monad.Trans.Except
-import Control.Monad.Trans.Cont
-import Control.Monad.Trans.Class
-import Control.Monad.IO.Class
+import System.Posix (Fd(..))
 
 data Termios = Termios
   { termios_iflag :: !Int
@@ -58,16 +48,6 @@ pattern ICANON = 0x0002
 pattern ECHO = 0x0008
 pattern IEXTEN = 0x8000
 
-data Signal = SIGINT | SIGQUIT | SIGTSTP | SIGWINCH
-
--- | On failure, returns the type of the JSVal.
-toJsObject :: JSVal -> IO (Either String JSObject)
-toJsObject jsv = do
-  ty <- jsTypeOf jsv
-  if ty == "object"
-    then pure $ Right (coerce jsv)
-    else pure (Left ty)
-
 jsTypeOf :: JSVal -> IO String
 jsTypeOf jsv = bracket
   (js_typeof jsv)
@@ -80,26 +60,8 @@ withJSString str k = bracket
   (freeJSVal . coerce)
   k
 
-installPtySignalHandler :: Pty -> (Signal -> IO ()) -> IO (IO ())
-installPtySignalHandler pty h = evalContT $ do
-  h1 <- ContT $ bracketOnError (js_to_jscallable $ h SIGINT) (freeJSVal . coerce)
-  h2 <- ContT $ bracketOnError (js_to_jscallable $ h SIGQUIT) (freeJSVal . coerce)
-  h3 <- ContT $ bracketOnError (js_to_jscallable $ h SIGTSTP) (freeJSVal . coerce)
-  h4 <- ContT $ bracketOnError (js_to_jscallable $ h SIGWINCH) (freeJSVal . coerce)
-  dispose <- ContT $ bracketOnError (js_pty_on_signal pty h1 h2 h3 h4) (freeJSVal . coerce)
-  pure (js_call_jsval dispose >> mapM_ (freeJSVal . coerce) [h1, h2, h3, h4])
-
--- | Get the 'Pty' given its identifier in globalThis
-getPty :: String -> IO (Either String Pty)
-getPty ident = evalContT $ runExceptT do
-
-  jsPty <- lift . ContT $ bracket (withJSString ident js_get_global) freeJSVal
-  jsPty' <- (liftIO $ toJsObject jsPty) >>= \case
-    Left ty -> throwE $ "globalThis." ++ ident ++ " expected to be an object, but is " ++ ty
-    Right o -> pure o
-  
-  -- TODO check jsPty' for available methods before coercing to Pty
-  pure $ coerce jsPty'
+onWindowChange :: Fd -> (IO ()) -> IO (IO ())
+onWindowChange (Fd fd) cb = undefined
 
 -- | Copies at most 'n' bytes of the JSByteArray to the given
 -- location in memory starting from index 0.
@@ -118,9 +80,9 @@ copyJSByteArray jsarr ptr n = do
           js_copy_to_byte_array jsarr' mem
           pure len
 
-getTermios :: Pty -> IO Termios
-getTermios pty = bracket
-  (js_pty_tcgets pty)
+getTermios :: Fd -> IO Termios
+getTermios (Fd fd) = bracket
+  (js_fd_ioctl_tcgets fd)
   (freeJSVal . coerce)
   \jts -> bracket
     (js_termios_cc jts)
@@ -137,7 +99,7 @@ getTermios pty = bracket
       ba' <- unsafeFreezeByteArray ba
       Termios <$> js_termios_iflag jts <*> js_termios_oflag jts <*> js_termios_cflag jts <*> js_termios_lflag jts <*> pure ba'
 
-setTermios :: Pty -> Termios -> IO ()
+setTermios :: Fd -> Termios -> IO ()
 setTermios pty Termios{..} = do
   let len = sizeofByteArray termios_cc
   allocaBytes len \ptr -> do
@@ -150,121 +112,8 @@ setTermios pty Termios{..} = do
         (freeJSVal . coerce)
         \jts -> js_pty_tcsets pty jts
 
-getWindowSize :: Pty -> IO (Int, Int)
+getWindowSize :: Fd -> IO (Int, Int)
 getWindowSize pty = bracket
   (js_pty_winsize pty)
   (freeJSVal . coerce)
   \ws -> (,) <$> js_winsize_col ws <*> js_winsize_row ws
-
-instance RawIO Pty where
-  read pty ptr _ n = do
-    readable <- js_pty_is_readable pty
-    if readable
-      then
-        bracket
-          (js_pty_read pty n)
-          (freeJSVal . coerce)
-          \jsarr -> copyJSByteArray jsarr ptr n
-      else do
-        readableMvar <- newEmptyMVar
-        bracket
-          (js_to_jscallable . void $ tryPutMVar readableMvar ())
-          (freeJSVal . coerce)
-          \cb ->
-            bracket
-              (js_pty_on_readable pty cb)
-              (\dispose -> js_call_jsval dispose >> freeJSVal (coerce dispose))
-              \_ -> takeMVar readableMvar
-        bracket
-          (js_pty_read pty n)
-          (freeJSVal . coerce)
-          \jsarr -> copyJSByteArray jsarr ptr n
- 
-  readNonBlocking pty ptr _ n = do
-    bracket
-      (js_pty_read pty n)
-      (freeJSVal . coerce)
-      \jsarr -> Just <$> copyJSByteArray jsarr ptr n
-
-  write pty ptr _ n = do
-    writable <- js_pty_is_writable pty
-    if writable
-      then do
-        bracket
-          (js_memory ptr n)
-          (freeJSVal . coerce)
-          \bytes -> js_pty_write pty bytes
-      else do
-        writableMvar <- newEmptyMVar
-        bracket
-          (js_to_jscallable . void $ tryPutMVar writableMvar ())
-          (freeJSVal . coerce)
-          \cb ->
-            bracket
-              (js_pty_on_writable pty cb)
-              (\dispose -> js_call_jsval dispose >> freeJSVal (coerce dispose))
-              \_ -> takeMVar writableMvar
-        bracket
-          (js_memory ptr n)
-          (freeJSVal . coerce)
-          \bytes -> js_pty_write pty bytes
-
-  writeNonBlocking pty ptr _ n = do
-    writable <- js_pty_is_writable pty
-    if writable
-      then do
-        bracket
-          (js_memory ptr n)
-          (freeJSVal . coerce)
-          \bytes -> js_pty_write pty bytes
-        pure n
-      else do
-        pure 0
-
-{-
-instance IODevice Pty where
-  ready pty writing msecs = do
-    let (isReady, onReady) =
-          if writing
-            then (js_pty_is_writable pty, js_pty_on_writable pty)
-            else (js_pty_is_readable pty, js_pty_on_readable pty)
-    r1 <- isReady
-    if r1
-      then pure True
-      else do
-        race_
-          (js_sleep msecs)
-          do
-            readyMvar <- newEmptyMVar
-            bracket
-              (js_to_jscallable (void $ tryPutMVar readyMvar ()))
-              (freeJSVal . coerce)
-              \cb -> 
-                bracket
-                  (onReady cb)
-                  (\dispose -> js_call_jsval dispose >> freeJSVal (coerce dispose))
-                  \_ -> takeMVar readyMvar
-        r2 <- isReady
-        pure r2
-  close _ = throwIO $ unsupportedOperation { ioe_location = "close @Pty" }
-  isTerminal _ = pure True
-  isSeekable _ = pure False
-  seek _ _ _ = throwIO $ unsupportedOperation { ioe_location = "seek @Pty" }
-  tell _ = throwIO $ unsupportedOperation { ioe_location = "tell @Pty" }
-  getSize _ = pure (-1) -- behaviour of instances of stdout, stdin
-  setSize _ _ = throwIO $ unsupportedOperation { ioe_location = "setSize @Pty" }
-  setEcho pty echo = do
-    ts <- getTermios pty
-    let ts' = ts { termios_lflag = if echo then termios_lflag ts .|. ECHO else termios_lflag ts .&. complement ECHO }
-    setTermios pty ts'
-  getEcho pty = do
-    ts <- getTermios pty
-    pure $ termios_lflag ts .&. ECHO /= 0
-  setRaw pty raw = do
-    ts <- getTermios pty
-    let ts' = ts { termios_lflag = if raw then termios_lflag ts .|. ICANON else termios_lflag ts .&. complement ICANON }
-    setTermios pty ts'
-  devType _ = pure Stream
-  dup _ = throwIO $ unsupportedOperation { ioe_location = "dup @Pty" }
-  dup2 _ _ = throwIO $ unsupportedOperation { ioe_location = "dup2 @Pty" }
--}
